f9c05fdff75a6bcd2ea382ef1015a9b05ba31b7a
devark28 <e.shimwa1@alustudent.com>
2026-02-10 20:14:55 +0200

    add temperature monitoring functionality with LED indicator in main.c

---
+++ question_4/main.c
@@ -0,0 +1,29 @@
+const int sensorPin = A0;
+const int ledPin = 2;
+
+void setup()
+{
+    pinMode(ledPin, OUTPUT);
+    pinMode(sensorPin, INPUT);
+    Serial.begin(9600);
+}
+
+void loop()
+{
+    // read analog input through analog-digital converter
+    int adc = analogRead(sensorPin);
+    // divide by adc range and multiply max voltage range
+    float voltage = adc * (5.0 / 1023.0);
+    // remove the base/offset voltage, multiply 100 to celcius range
+    float tempC = (voltage - 0.5) * 100.0;
+
+    // if temp gt ambient temperature
+    if (tempC > 28.0) {
+        digitalWrite(ledPin, HIGH);
+    } else {
+        digitalWrite(ledPin, LOW);
+    }
+
+    Serial.println(tempC);
+    delay(500);
+}
